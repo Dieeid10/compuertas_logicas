9b348b672b00787c3f633f688087640d10fb6c79
Diego <dieeid10@gmail.com>
2026-05-04 18:54:37 -0300

    Se modifica el titulo

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <main>
       <header 
-        className='bg-gray-800 text-white p-4 text-center rounded-b-lg shadow-md'
+        className='bg-gray-800 text-white p-4 text-center rounded-b-lg shadow-md flex flex-col gap-2 mb-6'
       >
         <h1
-          className=''
+          className='text-3xl'
         >Compuertas lógicas</h1>
         <p>Haz click para cambiar los switches de encendido y apagado.</p>
       </header>
